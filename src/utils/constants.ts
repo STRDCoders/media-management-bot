15a63c665eae598cb8ab4e18a654d213a3ed018c
Tomer Blecher <tomer.blecher.brolix@gmail.com>
2022-03-08 17:18:59 +0200

    Constants api details

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,19 @@
+export interface MediaServerLogin {
+  readonly host: string;
+  readonly apiKey: string;
+  readonly basePath: string;
+}
+
 export class Constants {
-  static readonly Rabbit = class {
-    static readonly uri = "amqp://localhost";
-  };
+  static readonly botToken: string = process.env.BOT_TOKEN!!;
+  static readonly sonarr: MediaServerLogin = Object.freeze({
+    host: process.env.SONARR_HOST!!,
+    basePath: "/api/v3",
+    apiKey: process.env.SONARR_API_KEY!!,
+  });
+  static readonly radarr: MediaServerLogin = Object.freeze({
+    host: process.env.RADARR_HOST!!,
+    basePath: "/api/v3",
+    apiKey: process.env.RADARR_API_KEY!!,
+  });
 }
