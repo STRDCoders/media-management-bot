2e9a1fc16599973791e701d343b212b2bb8797c2
brolix <tomer.blecher.brolix@gmail.com>
2022-05-14 15:26:46 +0300

    Detach mode

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker build --tag media-namagement-bot .
-sudo docker run --rm --network host --env-file /etc/media-management-bot/env media-namagement-bot
+sudo docker run -d --rm --network host --env-file /etc/media-management-bot/env media-namagement-bot
