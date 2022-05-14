750ea0121da66048a802cd606ac3ff33c65ff413
brolix <tomer.blecher.brolix@gmail.com>
2022-05-14 14:43:25 +0300

    Fix Delay status for queue items

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -16,6 +16,7 @@ export class Constants {
         description: (title: string) => `${title} - `,
         warning: "⚠️ Contact admin",
         downloading: (remainingTime: string) => `📥 Downloading(${remainingTime} remaining)`,
+        delay: "⏱️ Waiting for better quality(~12 hours)",
       }),
     }),
   });
